3a4649b9c3987771b7b3b4aa74386c3d3f896c89
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-18 18:15:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ names = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 
 def batch_badge_creator(names)
   names.each do |name|
-    return badge_maker(name)
+    return badge_maker(names)
   end
 end
 
