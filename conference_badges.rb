7c15d12e6dd80616ae155f1348970b88422ee2ee
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-18 17:54:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,4 @@ require 'pry'
 names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(names)
-  names.each do |name|
-  badge = "Hello, my name is #{name}"
 end
-return badge
-end
-
-badge_maker(names)
